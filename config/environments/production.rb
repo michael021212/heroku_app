49cdea6542395d880c70d95cb719a9b115a0a488
tanakashun <michael021212@gmail.com>
2019-12-31 14:07:23 +0900

    Write ' } '

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,11 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = [I18n.default_locale]
+  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
